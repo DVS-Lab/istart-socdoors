a4e8f119b44d4c7a3dfa6c18ab760dd962ba6a4c
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2021-11-23 08:42:31 -0500

    update templates

---
+++ code/extractROI.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+for sub in `cat ${maindir}/code/newsubs.txt`; do	
+	# ROI name and other path information
+	for ROI in NAcc; do
+		for TASK in doors socialdoors; do		
+			MASK=${maindir}/masks/seed-${ROI}.nii.gz
+			fslmeants -i /data/projects/istart-data/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz -m ${MASK} -o sub-${sub}_${ROI}.txt
+			mv sub-${sub}_${ROI}.txt ${maindir}/derivatives/fsl/sub-${sub}/
+		done
+	done
+done
