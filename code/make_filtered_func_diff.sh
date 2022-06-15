742515ead1a5002c231537de37425f3aa9df664d
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2022-06-15 18:00:00 -0400

    edit script to run for robust regression models

--- code/make_filtered_func_diff.sh
+++ code/make_filtered_func_diff.sh
@@ -14,12 +14,12 @@ for modelnum in 2 3; do
 	#mkdir ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n92_flame1+2_randomise/L3_model-${modelnum}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_flame1+2_randomise
 	
 	#if [${robust} == "no"]; then
-	fslmaths ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n46_flame1+2/L3_task-socialdoors_type-${type}_cnum-4_cname-win-loss_flame1+2.gfeat/cope1.feat/filtered_func_data.nii.gz	-sub ${MAINDIR}/L3_model-${modelnum}_task-doors_n46_flame1+2/L3_task-doors_type-${type}_cnum-4_cname-win-loss_flame1+2.gfeat/cope1.feat/filtered_func_data.nii.gz type-${type}_model-${modelnum}_randomise_filteredfunc_diff;
-	mv type-${type}_model-${modelnum}_randomise_filteredfunc_diff.nii.gz ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n92_flame1+2_randomise/L3_model-${modelnum}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise;
+	#fslmaths ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n46_flame1+2/L3_task-socialdoors_type-${type}_cnum-4_cname-win-loss_flame1+2.gfeat/cope1.feat/filtered_func_data.nii.gz	-sub ${MAINDIR}/L3_model-${modelnum}_task-doors_n46_flame1+2/L3_task-doors_type-${type}_cnum-4_cname-win-loss_flame1+2.gfeat/cope1.feat/filtered_func_data.nii.gz type-${type}_model-${modelnum}_randomise_filteredfunc_diff;
+	#mv type-${type}_model-${modelnum}_randomise_filteredfunc_diff.nii.gz ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n92_flame1+2_randomise/L3_model-${modelnum}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise;
 	
 	#elif [${robust} == "yes"]; then	
-	#fslmaths ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n46_flame1+2/L3_task-socialdoors_type-${type}_cnum-4_cname-win-loss_flame1+2_robust.gfeat/cope1.feat/filtered_func_data.nii.gz	-sub ${MAINDIR}/L3_model-${modelnum}_task-doors_n46_flame1+2/L3_task-doors_type-${type}_cnum-4_cname-win-loss_flame1+2_robust.gfeat/cope1.feat/filtered_func_data.nii.gz type-${type}_model-${modelnum}_randomise_filteredfunc_diff
-	#mv type-${type}_model-${modelnum}_randomise_filteredfunc_diff.nii.gz ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n92_flame1+2_randomise/L3_model-${modelnum}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise_robust
+	fslmaths ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n46_flame1+2/L3_task-socialdoors_type-${type}_cnum-4_cname-win-loss_flame1+2_robust.gfeat/cope1.feat/filtered_func_data.nii.gz	-sub ${MAINDIR}/L3_model-${modelnum}_task-doors_n46_flame1+2/L3_task-doors_type-${type}_cnum-4_cname-win-loss_flame1+2_robust.gfeat/cope1.feat/filtered_func_data.nii.gz type-${type}_model-${modelnum}_randomise_filteredfunc_diff
+	mv type-${type}_model-${modelnum}_randomise_filteredfunc_diff.nii.gz ${MAINDIR}/L3_model-${modelnum}_task-socialdoors_n92_flame1+2_randomise/L3_model-${modelnum}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise_robust
 	
 	#fi
 
