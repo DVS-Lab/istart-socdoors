65bc29673bf9155c604c06d7f01fef6d5a216aaa
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2022-06-16 08:15:31 -0400

    add fslstats for randomise models & Mars TPJ masks

--- code/run_fslstats.sh
+++ code/run_fslstats.sh
@@ -5,7 +5,7 @@
 
 for model in 2 3; do
 	for type in "act" "nppi-dmn" "ppi_seed-VS_thr5"; do
-		for file in /data/projects/istart-socdoors/derivatives/fsl/L3_model-${model}_task-socialdoors_n92_flame1+2_randomise/L3_model-${model}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise_robust/*.nii.gz; do
+		for file in /data/projects/istart-socdoors/derivatives/fsl/L3_model-${model}_task-socialdoors_n92_flame1+2_randomise/L3_model-${model}_task-socialdoors_type-${type}_cnum-4_cname-win-loss_randomise/*.nii.gz; do
 			echo $file
 			fslstats $file -R
 		done
