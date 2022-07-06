c654d444ec0b78e7bb3618eea8e0e37341acf84c
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2022-07-05 22:02:16 -0400

    Update behavioral_analysis.m

--- code/behavioral_analysis.m
+++ code/behavioral_analysis.m
@@ -8,11 +8,11 @@ maindir = '/Users/jameswyngaarden/Documents/GitHub/istart/social_reward_c/data';
 warning off all
 
 % Specify subs
-subs = [1001, 1002, 1003, 1004, 1006, 1007, 1009, 1010, 1011, 1012, 1013, 1015, 1016, ... 
-    1019, 1021, 1240, 1242, 1243, 1244, 1245, 1247, 1248, 1249, 1251, 1253, 1255, 1276, ...
-    1282, 1286, 1294, 1300, 1301, 1302, 1303, 3101, 3116, 3122, 3125, 3140, ...
-    3143, 3152, 3164, 3166, 3167, 3170, 3173, 3175, 3176, 3186, 3189, 3190, ...
-    3199, 3200, 3206, 3210, 3212, 3218, 3220, 3223];
+subs = [1001, 1003, 1004, 1006, 1009, 1010, 1012, 1013, 1015, 1016, ... 
+    1019, 1021, 1242, 1243, 1244, 1245, 1247, 1248, 1249, 1251, 1255, 1276, ...
+    1286, 1294, 1301, 1302, 1303, 3116, 3122, 3125, 3140, ...
+    3143, 3152, 3166, 3167, 3170, 3173, 3175, 3176, 3189, 3190, ...
+    3199, 3200, 3206, 3212, 3220];
 
 % Create a framework for data_mat--X columns: sub ID, RT doors after win,
 % RT doors after loss, RT social after win, RT social after loss
@@ -136,19 +136,134 @@ data_mat.Properties.VariableNames(1:5)={'Sub','Doors_Win_RT','Doors_Loss_RT','So
 
 %% Plots
 
-% All four conditions
+% All four conditions -- Plot with groups
 figure
-bar([mean(data_mat.Doors_Win_RT);mean(data_mat.Doors_Loss_RT);mean(data_mat.Social_Win_RT);mean(data_mat.Social_Loss_RT)]);
-xticks(1:4);
-xticklabels({'Doors Win', 'Doors Loss', 'Social Win', 'Social Loss'});
-ylabel('RT (sec)');
+x = 1:2;
+bar_data = [mean(data_mat.Doors_Win_RT) mean(data_mat.Social_Win_RT); mean(data_mat.Doors_Loss_RT) mean(data_mat.Social_Loss_RT)];
+b=bar(x, bar_data, 'grouped');
+title('RT by task condition');
+xticks(1:2);
+xticklabels({'Wins', 'Losses'});
+ylabel('RT');
 xlabel('Condition');
-ylim([1.6 2.1]);
+ylim([1.6 2.4]);
 hold on
-e1=[(std(data_mat.Doors_Win_RT)/sqrt(length(data_mat.Doors_Win_RT))) (std(data_mat.Doors_Loss_RT)/sqrt(length(data_mat.Doors_Loss_RT))) (std(data_mat.Social_Win_RT)/sqrt(length(data_mat.Social_Win_RT))) (std(data_mat.Social_Loss_RT)/sqrt(length(data_mat.Social_Loss_RT))) ];
-errorbar(1:4, [mean(data_mat.Doors_Win_RT);mean(data_mat.Doors_Loss_RT);mean(data_mat.Social_Win_RT);mean(data_mat.Social_Loss_RT)], e1, 'ko');
+%legend('Doors', 'Social');
+[lgd, icons, plots, txt] = legend('show');
+e1=[(std(data_mat.Doors_Win_RT)/sqrt(length(data_mat.Doors_Win_RT))) (std(data_mat.Social_Win_RT)/sqrt(length(data_mat.Social_Win_RT))); (std(data_mat.Doors_Loss_RT)/sqrt(length(data_mat.Doors_Loss_RT))) (std(data_mat.Social_Loss_RT)/sqrt(length(data_mat.Social_Loss_RT))) ];
+ngroups = size(bar_data, 1);
+nbars = size(bar_data, 2);
+% Calculating the width for each bar group
+groupwidth = min(0.8, nbars/(nbars + 1.5));
+for i = 1:nbars
+    x = (1:ngroups) - groupwidth/2 + (2*i-1) * groupwidth / (2*nbars);
+    errorbar(x, bar_data(:,i), e1(:,i), 'ko');
+end
+b(1).FaceColor = '#77AC30';
+b(2).FaceColor = '#0072BD';
+b;
+hold off
 
+% Statistics
+disp('Doors Win ~ Social Win');
 [h,p,ci,stats]=ttest(data_mat.Doors_Win_RT,data_mat.Social_Win_RT);
+disp(h);
+disp(p);
+disp(ci);
+disp(stats);
+
+disp('Doors Win ~ Doors Loss');
+[h,p,ci,stats]=ttest(data_mat.Doors_Win_RT,data_mat.Doors_Loss_RT);
+disp(h);
+disp(p);
+disp(ci);
+disp(stats);
+
+disp('Social Win ~ Social Loss');
+[h,p,ci,stats]=ttest(data_mat.Social_Win_RT,data_mat.Social_Loss_RT);
+disp(h);
+disp(p);
+disp(ci);
+disp(stats);
+
+disp('Doors Loss ~ Social Loss');
+[h,p,ci,stats]=ttest(data_mat.Doors_Loss_RT,data_mat.Social_Loss_RT);
+disp(h);
+disp(p);
+disp(ci);
+disp(stats);
+
+% Combining across domain
+figure
+x = 1:2;
+data_mat.Doors=(data_mat.Doors_Win_RT+data_mat.Doors_Loss_RT)/2;
+data_mat.Social=(data_mat.Social_Win_RT+data_mat.Social_Loss_RT)/2;
+
+bar_data = [mean(data_mat.Doors); mean(data_mat.Social)];
+b=bar(x, bar_data, 'grouped');
+title('RT by domain');
+xticks(1:2);
+xticklabels({'Doors', 'Social'});
+ylabel('RT');
+xlabel('Condition');
+ylim([1.6 2.4]);
+hold on
+%legend('Doors', 'Social');
+%[lgd, icons, plots, txt] = legend('show');
+e1=[(std(data_mat.Doors)/sqrt(length(data_mat.Doors))); (std(data_mat.Social)/sqrt(length(data_mat.Social)))];
+ngroups = size(bar_data, 1);
+nbars = size(bar_data, 2);
+% Calculating the width for each bar group
+groupwidth = min(0.8, nbars/(nbars + 1.5));
+for i = 1:nbars
+    x = (1:ngroups) - groupwidth/2 + (2*i-1) * groupwidth / (2*nbars);
+    errorbar(x, bar_data(:,i), e1(:,i), 'ko');
+end
+%b(1).FaceColor = '#77AC30';
+%b(2).FaceColor = '#0072BD';
+b;
+hold off
+
+disp('Doors ~ Social');
+[h,p,ci,stats]=ttest(data_mat.Doors,data_mat.Social);
+disp(h);
+disp(p);
+disp(ci);
+disp(stats);
+
+% Combining across outcomes
+figure
+x = 1:2;
+data_mat.Wins=((data_mat.Doors_Win_RT+data_mat.Social_Win_RT)/2);
+data_mat.Losses=((data_mat.Doors_Loss_RT+data_mat.Social_Loss_RT)/2);
+
+bar_data = [mean(data_mat.Wins); mean(data_mat.Losses)];
+b=bar(x, bar_data, 'grouped');
+title('RT by outcome');
+xticks(1:2);
+xticklabels({'Wins', 'Losses'});
+ylabel('RT');
+xlabel('Condition');
+ylim([1.6 2.4]);
+hold on
+%legend('Doors', 'Social');
+%[lgd, icons, plots, txt] = legend('show');
+e1=[(std(data_mat.Wins)/sqrt(length(data_mat.Wins))); (std(data_mat.Losses)/sqrt(length(data_mat.Losses)))];
+ngroups = size(bar_data, 1);
+nbars = size(bar_data, 2);
+% Calculating the width for each bar group
+groupwidth = min(0.8, nbars/(nbars + 1.5));
+for i = 1:nbars
+    x = (1:ngroups) - groupwidth/2 + (2*i-1) * groupwidth / (2*nbars);
+    errorbar(x, bar_data(:,i), e1(:,i), 'ko');
+end
+%b(1).FaceColor = '#77AC30';
+%b(2).FaceColor = '#0072BD';
+b;
+hold off
+
+disp('Wins ~ Losses');
+[h,p,ci,stats]=ttest(data_mat.Wins,data_mat.Losses);
 disp(h);
 disp(p);
 disp(ci);
