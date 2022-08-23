68b6a89e21240055e1652863691680b4c86cffef
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2022-08-23 11:27:22 -0400

    update scatterplots

--- code/plot_scatterplots_15aug22.R
+++ code/plot_scatterplots_15aug22.R
@@ -25,39 +25,40 @@ socialanddoors_model2 <- read_excel("~/Documents/Documents_Air/Github/istart-soc
 
 # Socialdoors Model 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# TFCE Lateral Occipital Cortex
-model1 <- lm(aug22_task_socialdoors_model_4_type_nppi_dmn_cnum_4_thresh_tfce_corrp_tstat13 ~
+# Cluster tstat 8 (SU-neg) Lateral Occipital Cortex
+model1 <- lm(aug22_task_socialdoors_model_2_type_act_cnum_4_thresh_clustere_corrp_tstat8 ~
                tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq, data=socialdoors_model2)
-crPlots(model1)
+model1
+crPlots(model1, smooth=FALSE)
 
 # Socialdoors Model 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Cluster Cerebellum
+# Cluster tstat 13 (RT-pos) Cerebellum
 model2 <- lm(aug22_task_socialdoors_model_4_type_nppi_dmn_cnum_4_thresh_clustere_corrp_tstat13 ~
                tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq + RT, data=socialdoors_model4)
-crPlots(model2, order=2)
+crPlots(model2, smooth=FALSE)
 
-# Vox Cerebellum
-model3 <- lm(aug22_task_socialdoors_model_4_type_act_cnum_4_thresh_vox_corrp_tstat7 ~
-               tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq + RT, data=socialdoors_model4)
-crPlots(model3)
+# Vox tstat 13 (RT-pos) Cerebellum
+#model3 <- lm(aug22_task_socialdoors_model_4_type_act_cnum_4_thresh_vox_corrp_tstat7 ~
+#               tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq + RT, data=socialdoors_model4)
+#crPlots(model3)
 
 # Social + Doors Model 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Thresh zstat 4 (RS-neg) Precuneus
 model4 <- lm(aug22_task_socialanddoors_model_2_type_act_cnum_4_thresh_zstat4 ~
                tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq, data=socialanddoors_model2)
-crPlots(model4)
+crPlots(model4, smooth=FALSE)
 
 # Thresh zstat 7 (SU-pos) Lateral Occipital
 model5 <- lm(aug22_task_socialanddoors_model_2_type_act_cnum_4_thresh_zstat7 ~
                 tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq, data=socialanddoors_model2)
-crPlots(model5)
+crPlots(model5, smooth=FALSE)
 
 # Thresh zstat 8 (SU-neg) Occipital Pole 
 model6 <- lm(aug22_task_socialanddoors_model_2_type_nppi_dmn_cnum_4_thresh_zstat8 ~
                 tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq, data=socialanddoors_model2)
-crPlots(model6)
+crPlots(model6, smooth=FALSE)
 
 # Thresh zstat 5 (RS_sq-pos) Middle Frontal Gyrus
 model7 <- lm(aug22_task_socialanddoors_model_2_type_ppi_seed_VS_thr5_cnum_4_thresh_zstat5 ~
@@ -67,29 +68,29 @@ crPlots(model7)
 # Thresh zstat 10 (RT-neg) Precuneus
 model7b <- lm(aug22_task_socialanddoors_model_2_type_act_cnum_4_thresh_zstat10_cluster_7 ~
                tsnr + fd_mean + RS + RS_square + SU + SUxRS + SUxRS_sq, data=socialanddoors_model2)
-crPlots(model7b, smooth=TRUE)
+crPlots(model7b, smooth=FALSE)
 
 # Social + Doors Model 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Thresh zstat 3 (RS-pos) Middle Temporal Gyrus
 model8 <- lm(aug22_task_socialanddoors_model_3_type_act_cnum_4_thresh_zstat3 ~
                tsnr + fd_mean + RS + RS_square + SU, data=socialanddoors_model3)
-crPlots(model8)
+crPlots(model8, smooth=FALSE)
 
 # Thresh zstat 7 (SU-pos) Lateral Occipital
 model9 <- lm(aug22_task_socialanddoors_model_3_type_act_cnum_4_thresh_zstat7 ~
                 tsnr + fd_mean + RS + RS_square + SU, data=socialanddoors_model3)
-crPlots(model9)
+crPlots(model9, smooth=FALSE)
 
-# Thresh zstat 4 (RS-neg) Postcentral Gyrus
+# Thresh zstat 3 (RS-pos) Postcentral Gyrus
 model10 <- lm(aug22_task_socialanddoors_model_3_type_ppi_seed_VS_thr5_cnum_4_thresh_zstat3 ~
                 tsnr + fd_mean + RS + RS_square + SU, data=socialanddoors_model3)
-crPlots(model10)
+crPlots(model10, smooth=FALSE)
 
 # Thresh zstat 7 (SU-neg) Cingulate Gyrus
 model11 <- lm(aug22_task_socialanddoors_model_3_type_ppi_seed_VS_thr5_cnum_4_thresh_zstat7 ~
                  tsnr + fd_mean + RS + RS_square + SU, data=socialanddoors_model3)
-crPlots(model11)
+crPlots(model11, smooth=FALSE)
 
 # Social + Doors Model 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -111,20 +112,20 @@ crPlots(model13)
 # Thresh zstat 10 (RT-neg) Juxtapositional Cortex
 model14 <- lm(aug22_task_socialanddoors_model_5_type_act_cnum_4_thresh_zstat10 ~
                 tsnr + fd_mean + RS + RS_square + SU + RT, data=socialanddoors_model5)
-crPlots(model14)
+crPlots(model14, smooth=FALSE)
 
 # Thresh zstat 7 (SU-pos) Thalamus
 model15 <- lm(aug22_task_socialanddoors_model_5_type_nppi_dmn_cnum_4_thresh_zstat7 ~
                 tsnr + fd_mean + RS + RS_square + SU + RT, data=socialanddoors_model5)
-crPlots(model15)
+crPlots(model15, smooth=FALSE)
 
 # Thresh zstat 3 (RS-pos) Postcentral Gyrus
 model16 <- lm(aug22_task_socialanddoors_model_5_type_ppi_seed_VS_thr5_cnum_4_thresh_zstat3 ~
                 tsnr + fd_mean + RS + RS_square + SU + RT, data=socialanddoors_model5)
-crPlots(model16)
+crPlots(model16, smooth=FALSE)
 
 # Thresh zstat 7 (SU-pos) Intracalcarine Cortex
 model17 <- lm(aug22_task_socialanddoors_model_5_type_ppi_seed_VS_thr5_cnum_4_thresh_zstat7 ~
                  tsnr + fd_mean + RS + RS_square + SU + RT, data=socialanddoors_model5)
-crPlots(model17)
+crPlots(model17, smooth=FALSE)
 
