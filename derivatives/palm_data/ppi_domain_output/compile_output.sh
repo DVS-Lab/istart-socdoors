efcec4c371ce739d64103623b67e13dbfd2ed933
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2022-11-10 13:29:56 -0500

    run corrected palm with -ise option

---
+++ derivatives/palm_data/ppi_domain_output/compile_output.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# compile PALM output into a single file
+
+model=ppi_domain
+
+for c in 1 2 3 4 5 6 7 8 9 10 11 12; do
+	for s in tstat tstat_uncp tstat_fwep tstat_cfwep; do
+
+		echo ${model}_dat_${s}_c${c}.csv
+		cat ${model}_dat_${s}_c${c}.csv
+	
+	done
+done
