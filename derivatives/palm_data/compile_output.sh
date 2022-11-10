cad129f58e10f7a567241eb126a112ca4da21d9c
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2022-11-10 13:31:18 -0500

    clean git

--- derivatives/palm_data/compile_output.sh
+++ derivatives/palm_data/compile_output.sh
@@ -2,9 +2,13 @@
 
 # compile PALM output into a single file
 
-for i in *.csv; do
+model=act_outcome
 
-	echo $i
-	cat $i
+for c in 1 2 3 4 5 6 7 8 9 10 11 12; do
+	for s in tstat tstat_uncp tstat_fwep tstat_cfwep; do
 
+		echo ${model}_dat_${s}_c${c}.csv
+		cat ${model}_dat_${s}_c${c}.csv
+	
+	done
 done
