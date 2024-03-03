93c434838ffc4daf2bd6335b40420a6f77b1a325
jameswyngaarden <james.wyngaarden@temple.edu>
2024-03-03 11:23:06 -0500

    check extractions for deception analyses

--- code/extractROI_PALM.sh
+++ code/extractROI_PALM.sh
@@ -8,14 +8,14 @@ outputdir=${maindir}/derivatives/imaging_plots
 # base paths
 for TASK in doors socialdoors; do
 	for TYPE in act ppi_seed-VS_thr5; do
-		for COPEINFO in "1 win" "2 loss"; do
+		for COPEINFO in "1 win" "2 loss" "4 win-loss"; do
 			set -- $COPEINFO
 			COPENUM=$1
 			COPENAME=$2
 			
 			DATA=`ls -1 ${maindir}/derivatives/fsl/L3_model-1_task-${TASK}_n46_flame1+2/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_cname-${COPENAME}_flame1+2.gfeat/cope1.feat/filtered_func_data.nii.gz`
 			
-			fslmeants -i $DATA -o ${outputdir}/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_cname-${COPENAME}_flame1+2_4Nov22.txt
+			fslmeants -i $DATA -o ${outputdir}/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_cname-${COPENAME}_flame1+2_3mar24.txt
 		done
 	done
 done
