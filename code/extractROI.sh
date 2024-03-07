3f558dfb633b464d220f70e4b8e04ccc7a9198fc
jameswyngaarden <james.wyngaarden@temple.edu>
2024-03-07 11:00:21 -0500

    extract zstats

--- code/extractROI.sh
+++ code/extractROI.sh
@@ -6,12 +6,12 @@ maindir="$(dirname "$scriptdir")"
 
 for sub in `cat ${maindir}/code/newsubs.txt`; do	
 	# ROI name and other path information
-	for ROI in seed-VS_thr5; do
+	for ROI in VS_thr5; do
 		for TASK in doors socialdoors; do		
-			MASK=${maindir}/masks/${ROI}.nii.gz
-			#MASK=${maindir}/masks/seed-${ROI}.nii.gz
-			fslmeants -i /data/projects/istart-data/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${TASK}_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz -m ${MASK} -o sub-${sub}_task-${TASK}_run-1_${ROI}.txt
-			mv sub-${sub}_task-${TASK}_run-1_${ROI}.txt ${maindir}/derivatives/fsl/sub-${sub}/
+			#MASK=${maindir}/masks/${ROI}.nii.gz
+			MASK=${maindir}/masks/seed-${ROI}.nii.gz
+			fslmeants -i /ZPOOL/data/projects/istart-socdoors/derivatives/fsl/sub-${sub}/L1_task-${TASK}_model-1_type-act_run-1_sm-6.feat/stats/zstat4.nii.gz -m ${MASK} -o sub-${sub}_task-${TASK}_run-1_${ROI}_zstat4.txt
+			mv sub-${sub}_task-${TASK}_run-1_${ROI}.txt ${maindir}/derivatives/zstats/
 		done
 	done
 done
